538ca1c1f4e2aceb6874d7552d4d1f23f6cee589
mlakka <104219935+mlakka@users.noreply.github.com>
2022-04-24 12:18:57 -0700

    Added docker-compose for multi-container deployment

--- diag-service/index.js
+++ diag-service/index.js
@@ -7,6 +7,14 @@ register.setDefaultLabels({
     app: 'diag-service'
 })
 
+client.collectDefaultMetrics({
+    app: 'diag-service',
+    prefix: 'node_',
+    timeout: 10000,
+    gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
+    register
+});
+
 const app = express();
 const port = 8000;
 
